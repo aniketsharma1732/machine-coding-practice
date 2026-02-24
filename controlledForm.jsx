c0522e53b46226d2220af03bcaf727173b9ae73d
Aniket Sharma <anikets.5061@gmail.com>
2026-02-24 23:41:14 +0530

    controlledForms

---
+++ controlledForm.jsx
@@ -0,0 +1,53 @@
+// 4.	Build a controlled form
+// → multiple inputs, validation
+
+import react, { useState } from "react"
+
+const controlledForm = () => {
+
+    const [formData, setFormData] = useState({
+        name: " ",
+        email: " "
+    });
+   // const [error, setError] = useState({});
+
+
+    function handleChange(e) {
+
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+
+
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        alert(`Name: ${form.name}, Email: ${form.email}`);
+    }
+
+    return (
+
+        <div>
+            <form action="put" onChange={handleSubmit}>
+
+                <input
+                    type="text"
+                    value={formData.data}
+                    onChange={handleChange}
+                    placeholder="enter your name"
+                />
+                <input
+                    type="text"
+                    value={formData.data}
+                    onChange={handleChange}
+                    placeholder="enter your school name"
+                />
+
+                <button type="submit">submit</button>
+            </form>
+
+        </div>
+
+    );
+}
+
+export default controlledForm;
